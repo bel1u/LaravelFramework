91e7b59d17622286923a6deccf9b8f1e5da01c27
is41-belyysa <belyxao@mail.ru>
2022-04-16 14:39:52 +0500

    Задание 16.04

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//1
+Route::get("my-name", function (){
+    return "Муниров Андрей Анатольевич";
+});
+//2
+Route::get("my-friend", function (){
+    return "Григорьев";
+});
+//3
+Route::get('get-friend/{name}', function ($name){
+    return $name;
+});
+//4
+Route::get('my-city/{city}', function ($city){
+    return $city;
+})->where('city', '[A-za-z]+');
+Route::get('level/lvl', function ($lvl){
+    if($lvl)
+});
+//9
+Route::get()
